271c282bec40cc4e2fb6f9ffa5b232ad202fc7f6
pavani <pavaniavni456@gmail.com>
2022-02-10 11:49:15 +0530

    added threadex

---
+++ ThreadEx/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Threading
+{
+    internal class ExampleofThread
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(MethodThread);
+            Console.WriteLine(thread.ThreadState);
+            thread.Start();
+            Console.WriteLine(thread.ThreadState);
+            thread.Join();
+            Console.WriteLine(thread.ThreadState);
+            Console.WriteLine("Work completed");
+        }
+
+        static void MethodThread()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Work in progress");
+            }
+        }
+    }
+}
